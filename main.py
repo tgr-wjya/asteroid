f807ca203ddf5d43fbf4a7db3aaa1aef5c91e7b2
Tegar Wijaya Kusuma <64220909+tgr-wjya@users.noreply.github.com>
2026-09-20 21:52:48 +0700

    B: Add logging functionality for game state and events

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 print("Screen width:", SCREEN_WIDTH)
 print("Screen height:", SCREEN_HEIGHT)
+
+def main(color):
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+  while True:
+    log_state()
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        return
+
+    screen.fill("black")
+
+    pygame.display.flip()
+
+main("black")
